9043a48e7a8a20e18ea8af7c2f13c810865cf921
Parfait2026 <2547117@cegeplimoilou.ca>
2026-03-05 17:41:09 -0500

    Créer la classe Algue
    Contient uniquement un méthode toString()

---
+++ src/parfait_mbega/algues/Algue.java
@@ -0,0 +1,17 @@
+/**
+ * 420-266-H26 – POO II
+ * TP1 - Heritage
+ * Nom : MBEGA BILOA
+ * Prénom: Parfait Antoine
+ */
+
+package parfait_mbega.algues;
+
+
+public class Algue {
+
+    @Override
+    public String toString() {
+        return "Algue{}";
+    }
+}
